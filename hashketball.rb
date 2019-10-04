6a44e6b67ae3049bfcaa46b4176c3ae4eac27b66
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-04 03:28:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,11 +29,6 @@ def game_hash
 end
 
 
-# def num_points_scored(players)
-#   game_hash.each do ||
-#
-#   end
-
 
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
@@ -48,42 +43,42 @@ end
 
 
 
-def shoe_size
-
-end
-
-def team_colors
-
-end
-
-def team_names
-
-end
-
-def player_numbers
-
-end
-
-def player_stats
-
-end
-
-def big_shoe_rebounds
-
-end
-
-def most_points_scored
-
-end
-
-def winning_team
-
-end
-
-def player_with_longest_name
-
-end
-
-def long_name_steals_a_ton
-
-end
+# def shoe_size
+# 
+# end
+# 
+# def team_colors
+# 
+# end
+# 
+# def team_names
+# 
+# end
+# 
+# def player_numbers
+# 
+# end
+# 
+# def player_stats
+# 
+# end
+# 
+# def big_shoe_rebounds
+# 
+# end
+# 
+# def most_points_scored
+# 
+# end
+# 
+# def winning_team
+# 
+# end
+# 
+# def player_with_longest_name
+# 
+# end
+# 
+# def long_name_steals_a_ton
+# 
+# end
