e466e591d17e459744629b57b947e64514f31329
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-04 03:28:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -44,41 +44,41 @@ end
 
 
 # def shoe_size
-# 
+#
 # end
-# 
+#
 # def team_colors
-# 
+#
 # end
-# 
+#
 # def team_names
-# 
+#
 # end
-# 
+#
 # def player_numbers
-# 
+#
 # end
-# 
+#
 # def player_stats
-# 
+#
 # end
-# 
+#
 # def big_shoe_rebounds
-# 
+#
 # end
-# 
+#
 # def most_points_scored
-# 
+#
 # end
-# 
+#
 # def winning_team
-# 
+#
 # end
-# 
+#
 # def player_with_longest_name
-# 
+#
 # end
-# 
+#
 # def long_name_steals_a_ton
-# 
+#
 # end
